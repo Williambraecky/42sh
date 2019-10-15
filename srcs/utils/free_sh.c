38a174dea719b3d971ca92366019f375369ecf31
William Braeckman <wbraeckm@student.s19.be>
2019-10-15 19:51:55 +0200

    Reset signals on exit

--- srcs/utils/free_sh.c
+++ srcs/utils/free_sh.c
@@ -6,7 +6,7 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 17:49:40 by wbraeckm          #+#    #+#             */
-/*   Updated: 2019/10/08 14:42:59 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2019/10/15 16:19:14 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,19 @@
 
 void	free_sh(t_sh *shell)
 {
+	struct sigaction action;
+
 	ft_mapdel(shell->env);
 	if (shell->prompt_mode)
+	{
 		tcsetattr(SH_IN, TCSADRAIN, &shell->old_termios);
+		ft_memset(&action, 0, sizeof(action));
+		action.sa_handler = SIG_DFL;
+		sigaction(SIGINT, &action, NULL);
+		signal(SIGQUIT, SIG_DFL);
+		signal(SIGTSTP, SIG_DFL);
+		signal(SIGTTIN, SIG_DFL);
+		signal(SIGTTOU, SIG_DFL);
+		signal(SIGCHLD, SIG_DFL);
+	}
 }
