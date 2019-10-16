36c19d5d12c172a300295c64e17f4e0b07f54c11
William Braeckman <wbraeckm@student.s19.be>
2019-10-16 23:20:08 +0200

    Fix wstrindex function

--- libft/srcs/wstr/ft_wstrindex.c
+++ libft/srcs/wstr/ft_wstrindex.c
@@ -6,7 +6,7 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/11 16:30:26 by wbraeckm          #+#    #+#             */
-/*   Updated: 2019/10/11 16:31:25 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2019/10/16 19:49:16 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,15 @@ char	*ft_wstrindex(char *str, size_t index)
 	size_t	len;
 
 	len = 0;
+	if (index == 0)
+		return (str);
 	while (*str)
 	{
-		if ((*str & 0xC0) != 0x80)
-			len++;
 		if (len == index)
 			return (str);
+		if ((*str & 0xC0) != 0x80)
+			len++;
 		str++;
 	}
-	return (NULL);
+	return (str);
 }
