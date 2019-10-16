be7b6d4926788e08f729aebf749a8fb58ae53d7f
William Braeckman <wbraeckm@student.s19.be>
2019-10-16 23:19:42 +0200

    Implement strlen without color codes

---
+++ srcs/utils/strlen_nocolor.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strlen_nocolor.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/16 16:26:56 by wbraeckm          #+#    #+#             */
+/*   Updated: 2019/10/16 16:29:55 by wbraeckm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "sh.h"
+
+static size_t	skip_color_code(char *str, size_t pos)
+{
+	size_t len;
+
+	len = pos + 1;
+	if (str[len++] != '[')
+		return (1);
+	while (isdigit(str[len]))
+		len++;
+	if (str[len++] != 'm')
+		return (1);
+	return (len - pos);
+}
+
+size_t			strlen_nocolor(char *str)
+{
+	size_t i;
+	size_t len;
+
+	i = 0;
+	len = 0;
+	while (str[i])
+	{
+		if (str[i] == 0x1B)
+			i += skip_color_code(str, i);
+		else
+		{
+			len++;
+			i++;
+		}
+	}
+	return (len);
+}
