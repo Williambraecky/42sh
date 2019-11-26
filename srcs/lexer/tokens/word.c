348966c36cac67f02450bc696471584d42d0e531
Tchoupe <ntom@student.s19.be>
2019-11-26 14:27:35 +0100

    Corrected word charset

--- srcs/lexer/tokens/word.c
+++ srcs/lexer/tokens/word.c
@@ -6,7 +6,7 @@
 /*   By: ntom <ntom@student.s19.be>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/26 12:15:50 by ntom              #+#    #+#             */
-/*   Updated: 2019/11/26 13:56:39 by ntom             ###   ########.fr       */
+/*   Updated: 2019/11/26 14:26:59 by ntom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,22 @@
 
 static int	is_word_charset(char c)
 {
-	return (c == ' ' || c == '\t' || c == '\f' || c == '\r' || c == '\v'
-		|| c == '&' || c == '>' || c == '<' || c == '|' || c == '"'
-		|| c == '\n' || c == '\'' || c == ';');
-}
+	const char	*charset = " \t\f\r&><|\"\n';";
+	int			i;
 
-// Add macro with charset to not forget to add tokens in the future?
+	i = 0;
+	while (charset[i])
+	{
+		if (charset[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
 
 int			istok_word(t_lexer *lexer)
 {
-	return (is_word_charset(lexer->line[lexer->i]));
+	return (1);
 }
 
 int			createtok_word(t_lexer *lexer)
@@ -34,7 +40,9 @@ int			createtok_word(t_lexer *lexer)
 	len = 0;
 	tok.size = sizeof(t_token);
 	tok.type = T_WORD;
-	while (is_word_charset((lexer->line + lexer->i)[len]))
+	// TODO : add check stack
+	while (!(is_word_charset((lexer->line + lexer->i)[len]))
+		|| is_char_escaped(lexer->line, lexer->i + len))
 		len++;
 	if (!(tok.str = ft_strndup(lexer->line + lexer->i, len)))
 		return (SH_ERR_MALLOC);
