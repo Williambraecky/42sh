56e017fdbc536242f3f4d17055e8ce68f64e2329
William Braeckman <wbraeckm@student.s19.be>
2019-12-18 19:13:34 +0100

    Fix delimiter and add pwd

--- srcs/builtins/pwd.c
+++ srcs/builtins/pwd.c
@@ -6,11 +6,12 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/16 18:24:02 by wbraeckm          #+#    #+#             */
-/*   Updated: 2019/12/16 18:38:00 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2019/12/18 19:13:26 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
+#include <sys/param.h>
 
 /*
 ** NOTE: simply prints pwd; should use from env but if it's not present getcwd
@@ -18,8 +19,28 @@
 
 int		pwd_builtin(int argc, char **argv, t_sh *shell)
 {
+	char	*pwd;
+	char	buffer[MAXPATHLEN + 1];
+
 	(void)argc;
 	(void)argv;
-	(void)shell;
+	buffer[0] = '\0';
+	pwd = NULL;
+	if (get_env(shell, "PWD", &pwd) == SH_SUCCESS && access(pwd, F_OK) == 0)
+		ft_strlcat(buffer, pwd, MAXPATHLEN + 1);
+	else
+	{
+		if (!getcwd(buffer, MAXPATHLEN + 1))
+		{
+			ft_dprintf(2, "pwd: getcwd error\n");
+			return (1);
+		}
+	}
+	if (access(buffer, F_OK) == -1)
+	{
+		ft_dprintf(2, "pwd: %s: No such file or directory\n", buffer);
+		return (1);
+	}
+	ft_printf("%s\n", buffer);
 	return (0);
 }
