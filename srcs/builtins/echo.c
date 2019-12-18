96e94926f2c3b299beffd770fd5128f5f883b673
William Braeckman <wbraeckm@student.s19.be>
2019-12-18 16:19:35 +0100

    Changes to echo

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/16 18:19:47 by wbraeckm          #+#    #+#             */
-/*   Updated: 2019/12/18 16:13:43 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2019/12/18 16:19:22 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,13 @@
 int		echo_builtin(int argc, char **argv, t_sh *shell)
 {
 	size_t	i;
+	int		n_flag;
 
 	(void)shell;
-	i = 1;
+	n_flag = 0;
 	if (argc >= 2 && ft_strequ(argv[1], "-n"))
-		i = 2;
+		n_flag = 1;
+	i = 1;
 	while (i < (size_t)argc)
 	{
 		write(1, argv[i], ft_strlen(argv[i]));
@@ -31,6 +33,7 @@ int		echo_builtin(int argc, char **argv, t_sh *shell)
 		if (i != (size_t)argc)
 			write(1, " ", 1);
 	}
-	write(1, "\n", 1);
+	if (!n_flag)
+		write(1, "\n", 1);
 	return (0);
 }
