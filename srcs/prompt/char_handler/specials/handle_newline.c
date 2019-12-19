461efaad8760cde6e8a4d2245de48817950fcec0
William Braeckman <wbraeckm@student.s19.be>
2019-12-19 15:04:05 +0100

    Append newline to prompt return

--- srcs/prompt/char_handler/specials/handle_newline.c
+++ srcs/prompt/char_handler/specials/handle_newline.c
@@ -6,7 +6,7 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/23 15:53:47 by wbraeckm          #+#    #+#             */
-/*   Updated: 2019/12/08 16:43:28 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2019/12/19 14:58:30 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@
 
 int	handle_newline(t_prompt *prompt, char *buffer)
 {
-	(void)prompt;
 	(void)buffer;
+	if (buff_insert(&prompt->buffer, "\n", ft_strlen(prompt->buffer.buffer))
+			!= SH_SUCCESS)
+		return (SH_ERR_MALLOC);
 	return (RET_NL);
 }
