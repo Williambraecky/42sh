8eae3a5c2f0cdaecd539b1b5deaf26f7c7b9a0d5
Waberi Daher ahmed <wdaher-a@w-r6-p7.s19.be>
2019-12-19 18:53:42 +0100

    Adapted set_builtin argument syntax. Only name=value is accepted.

--- srcs/builtins/set.c
+++ srcs/builtins/set.c
@@ -19,18 +19,25 @@
 static int valid_arg(char *string)
 {
 		if (ft_strchr(string, '='))
-			return (0);
-		else
 			return (1);
+		else
+			return (0);
 }
 
 int				set_builtin(int argc, char **argv, t_sh *shell)
 {
+	char **av;
+
 	if (argc == 1)
 		ft_mapprint(shell->internals);
-	else if (argc == 2 && valid_arg(argv[1]) && has_internal(shell, argv[1]))
-		return (repl_internal(shell, argv[1], argv[2]));
-	else if (argc == 2 && valid_arg(argv[1]) && !has_internal(shell, argv[1]))
-		return (add_internal(shell, argv[1], argv[2]));
+	if (argc == 2 && valid_arg(argv[1]))
+	{
+		av = ft_strsplit(argv[1], '=');
+		if (argc == 2 && has_internal(shell, av[0]))
+			return (repl_internal(shell, av[0], av[1));
+		else if (argc == 2 && !has_internal(shell, av[0]))
+			return (add_internal(shell, av[0], av[1]));
+		ft_freesplit(av);
+	}
 	return (0);
 }
