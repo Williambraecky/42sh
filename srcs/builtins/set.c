0b0e0ecbba67514eafe71adbbed725c441236353
Waberi Daher ahmed <wdaher-a@w-r6-p7.s19.be>
2019-12-19 18:36:13 +0100

    Started working on set_builtin.

--- srcs/builtins/set.c
+++ srcs/builtins/set.c
@@ -16,10 +16,21 @@
 ** NOTE: defines internal variable + other
 */
 
-int		set_builtin(int argc, char **argv, t_sh *shell)
+static int valid_arg(char *string)
 {
-	(void)argc;
-	(void)argv;
-	(void)shell;
+		if (ft_strchr(string, '='))
+			return (0);
+		else
+			return (1);
+}
+
+int				set_builtin(int argc, char **argv, t_sh *shell)
+{
+	if (argc == 1)
+		ft_mapprint(shell->internals);
+	else if (argc == 2 && valid_arg(argv[1]) && has_internal(shell, argv[1]))
+		return (repl_internal(shell, argv[1], argv[2]));
+	else if (argc == 2 && valid_arg(argv[1]) && !has_internal(shell, argv[1]))
+		return (add_internal(shell, argv[1], argv[2]));
 	return (0);
 }
