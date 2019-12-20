124fd057a794a996012bf39d4dc7dc65f1669e35
Tchoupe <ntom@student.s19.be>
2019-12-20 11:52:40 +0100

    Fixed compilation bug in set builtin

--- srcs/builtins/set.c
+++ srcs/builtins/set.c
@@ -6,7 +6,7 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/16 18:15:22 by wbraeckm          #+#    #+#             */
-/*   Updated: 2019/12/16 18:38:21 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2019/12/20 11:52:12 by ntom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ int				set_builtin(int argc, char **argv, t_sh *shell)
 	{
 		av = ft_strsplit(argv[1], '=');
 		if (argc == 2 && has_internal(shell, av[0]))
-			return (repl_internal(shell, av[0], av[1));
+			return (repl_internal(shell, av[0], av[1]));
 		else if (argc == 2 && !has_internal(shell, av[0]))
 			return (add_internal(shell, av[0], av[1]));
 		ft_freesplit(av);
