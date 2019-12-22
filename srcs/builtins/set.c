604ab2116870a912d9dcfa85ffce682fe7031bc6
o/ <berry@Waberis-MacBook-Pro.local>
2019-12-22 17:09:09 +0100

    Added static print function for set_builtins.

--- srcs/builtins/set.c
+++ srcs/builtins/set.c
@@ -6,7 +6,7 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/16 18:15:22 by wbraeckm          #+#    #+#             */
-/*   Updated: 2019/12/20 11:52:12 by ntom             ###   ########.fr       */
+/*   Updated: 2019/12/22 16:57:11 by wdaher-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,49 @@
 ** NOTE: defines internal variable + other
 */
 
+/*
+** static int	is_word_charset(char c)
+** {
+** 	const char	*charset = " \t\f\r&><|\"\n';";
+** 	int			i;
+**
+** 	i = 0;
+** 	while (charset[i])
+** 	{
+** 		if (charset[i] == c)
+** 			return (1);
+** 		i++;
+** 	}
+** 	return (0);
+** }
+*/
+
 static int valid_arg(char *string)
 {
-		if (ft_strchr(string, '='))
-			return (1);
-		else
-			return (0);
+		return (ft_strchr(string, '=') != NULL);
 }
 
-int				set_builtin(int argc, char **argv, t_sh *shell)
+static void	print_internals(t_map *map)
+{
+	size_t	i;
+
+	if (!map)
+		return ;
+	i = 0;
+	while (i < map->max_size)
+	{
+		if (map->nodes[i].is_used)
+			ft_printf("%s\n", map->nodes[i].value);
+		i++;
+	}
+}
+
+int			set_builtin(int argc, char **argv, t_sh *shell)
 {
 	char **av;
 
 	if (argc == 1)
-		ft_mapprint(shell->internals);
+		print_internals(shell->internals);
 	if (argc == 2 && valid_arg(argv[1]))
 	{
 		av = ft_strsplit(argv[1], '=');
