451c69748477ffc2c516b1156da284818c776513
William Braeckman <wbraeckm@student.s19.be>
2019-12-26 21:54:55 +0100

    Reimplement &(<|>) format for lesserand and greaternad

--- srcs/lexer/tokens/greaterand.c
+++ srcs/lexer/tokens/greaterand.c
@@ -6,7 +6,7 @@
 /*   By: ntom <ntom@student.s19.be>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/12 16:46:46 by ntom              #+#    #+#             */
-/*   Updated: 2019/12/20 16:50:21 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2019/12/26 21:50:14 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 int			istok_greaterand(t_lexer *lexer, t_token *token)
 {
 	(void)lexer;
-	if (token->str[0] == '>' && token->str[1] == '&')
+	if ((token->str[0] == '>' && token->str[1] == '&') ||
+		(token->str[0] == '&' && token->str[1] == '>'))
 		return (1);
 	return (0);
 }
