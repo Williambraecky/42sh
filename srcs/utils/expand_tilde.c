0d1535f65d876644b2cbb5d3307fbfaae7b6d15d
Waberi Daher ahmed <wdaher-a@e-r2-p5.s19.be>
2020-01-10 17:00:10 +0100

    fixed norm on expand_tilde.c

--- srcs/utils/expand_tilde.c
+++ srcs/utils/expand_tilde.c
@@ -6,47 +6,40 @@
 /*   By: ntom <ntom@student.s19.be>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/10 10:42:42 by ntom              #+#    #+#             */
-/*   Updated: 2020/01/10 16:49:00 by wdaher-a         ###   ########.fr       */
+/*   Updated: 2020/01/10 16:59:32 by wdaher-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// TO DO :
-// only unquoted tilde
-// at the begining of the WORD
-// followed by all the characters before unquoted slash
-//			replace by $LOGNAME
-// or followed by all characters if no slash
-// 			replace by $HOME
-//
 
 #include "lexer.h"
 #include "sh.h"
 
 static int	replace_logname(t_sh *shell, char **str)
 {
-	char	tmp[PATH_MAX];
-  int   ret;
-  char  *tmp1;
+	char	*tmp;
+	int		ret;
+	char	*tmp1;
 
-  ft_bzero(tmp, PATH_MAX);
 	if ((ret = get_env(shell, "HOME", &tmp) > 0))
-    return (ret);
-  tmp1 = ft_strrchr(tmp, '/');
-  if (tmp1)
-    ft_strcpy(tmp1, (*str + 1));
-  return (SH_SUCCESS);
+		return (ret);
+	tmp = ft_strdup(tmp);
+	tmp1 = ft_strrchr(tmp, '/');
+	if (tmp1)
+		ft_strcpy(tmp1, (*str + 1));
+	free(tmp);
+	return (SH_SUCCESS);
 }
 
 static int	replace_home(t_sh *shell, char **str)
 {
-  char	tmp[PATH_MAX];
-  int   ret;
+	char	*tmp;
+	int		ret;
 
-  ft_bzero(tmp, PATH_MAX);
-  if ((ret = get_env(shell, "HOME", &tmp) > 0))
-    return (ret);
-  ft_strcpy(tmp, (*str + 1));
-  return (SH_SUCCESS);
+	if ((ret = get_env(shell, "HOME", &tmp) > 0))
+		return (ret);
+	tmp = ft_strdup(tmp);
+	ft_strcpy(tmp, (*str + 1));
+	free(tmp);
+	return (SH_SUCCESS);
 }
 
 int			replace_tilde(t_sh *shell, char **str)
