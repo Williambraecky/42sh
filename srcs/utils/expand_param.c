a38502352ac8a5f10416289aeea5674d67ec75f0
Tchoupe <ntom@student.s19.be>
2020-01-13 22:05:31 +0100

    Reworked param. In progress, no braces done.

--- srcs/utils/expand_param.c
+++ srcs/utils/expand_param.c
@@ -6,7 +6,7 @@
 /*   By: ntom <ntom@student.s19.be>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/10 15:23:53 by ntom              #+#    #+#             */
-/*   Updated: 2020/01/12 15:58:07 by ntom             ###   ########.fr       */
+/*   Updated: 2020/01/13 22:05:01 by ntom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,12 @@
 ** 			parameter set and not null,
 ** 			parameter set but null,
 ** 			parameter unset
+**	$parameter:			substitute parameter
+**	${#parameter}:		strlen
 ** 	${parameter:-word}:	substitute parameter/ substitute word/ substitute word
 ** 	${parameter:=word}:	substitute parameter/ assign word/ assign word
 ** 	${parameter:?word}:	substitute parameter/ error, exit/ error, exit
 ** 	${parameter:+word}:	substitute word/ substitute null/ substitute null
-** 	${#parameter}	strlen of parameter
 ** 	${parameter%}	replace smallest suffix
 ** 	${parameter%%}	replace largest suffix
 ** 	${parameter#}	replace smallest prefix
@@ -34,93 +35,106 @@
 */
 
 #define FIRST_CHAR 1
-#define NOT_FIRST_CHAR 0
 
 int		is_charset(char c, int first_char)
 {
 	return ((c >= 'a' && c <= 'z')
 			|| (c >= 'A' && c <= 'Z')
-			|| (c >= '0' && c <= '9' && first_char == NOT_FIRST_CHAR)
+			|| (c >= '0' && c <= '9' && first_char != FIRST_CHAR)
 			|| c == '_');
 }
 
-int		get_valid_parameter(t_sh *shell, char *str, size_t *len, char **result)
+int		get_valid_param(t_sh *shell, char *str, char **result, size_t *len)
 {
-	size_t	i;
-	char	tmp;
+	size_t	j;
 	int		ret;
+	char	tmp_c;
 
-	i = 0;
+	j = 0;
 	ret = SH_SUCCESS;
-	if (!(is_charset(str[i], FIRST_CHAR)))
-		return (ret);
-	i++;
-	while (str[i] && is_charset(str[i], NOT_FIRST_CHAR))
-		i++;
-	tmp = str[i];
-	str[i] = '\0';
-	*len = ft_strlen(str) + 1;
+	while (str[j] && is_charset(str[j], j))
+		j++;
+	tmp_c = str[j];
+	str[j] = '\0';
 	if ((ret = get_env(shell, str, result)) != SH_SUCCESS)
 		ret = get_internal(shell, str, result);
-	str[i] = tmp;
+	str[j] = tmp_c;
+	*len = j;
+	ft_printf("*len = %d, j = %d\n", *len, j);
 	return (ret);
 }
 
-int		expand_brace_param(t_sh *shell, char *str, char **result)
+int		join_expanded(char *str, size_t *dollar, char **result, size_t len)
 {
-	(void)shell;
-	(void)str;
-	(void)result;
+	char	tmp_c;
+	char	*tmp_str;
+
+	tmp_c = str[*dollar - 1];
+	str[*dollar - 1] = '\0';
+	tmp_str = ft_strjoin(str, *result);
+	str[*dollar - 1] = '$';
+	if (!tmp_str)
+		return (SH_ERR_MALLOC);
+	*result = tmp_str;
+	*result = ft_strjoin(tmp_str, str + *dollar + len);
+	ft_strdel(&tmp_str);
+	if (!*result)
+		return (SH_ERR_MALLOC);
+	//ft_printf("str = %s, result = %s, str+d+l = %s, len = %d\n", str, *result, str + *dollar + len, len);
 	return (SH_SUCCESS);
 }
 
-int		expand_no_brace(t_sh *shell, char *str, char **result)
+int		expand_brace(t_sh *shell, char *str, char **result, size_t *i)
 {
-	char	*expanded;
-	size_t	key_len;
+	(void)str;
+	(void)shell;
+	(void)result;
+	(void)i;
+	return(SH_SUCCESS);
+}
+
+int		expand_no_brace(t_sh *shell, char *str, char **result, size_t *len)
+{
+	char	*expand;
 	int		ret;
 
-	ret = SH_SUCCESS;
-	if ((ret = get_valid_parameter(shell, str + 1, &key_len, &expanded)
-		!= SH_SUCCESS))
-		return (ret);
-	if (!(*result = ft_strjoin(expanded, str + key_len)))
+	expand = NULL;
+	ret = get_valid_param(shell, str, &expand, len);
+	if (ret != SH_SUCCESS)
+		expand = "";
+	if (!(*result = ft_strdup(expand)))
 		return (SH_ERR_MALLOC);
-	return (SH_SUCCESS);
+	return(SH_SUCCESS);
 }
 
 int		expand_param(t_sh *shell, char *str, char **result)
 {
+	size_t	i;
+	size_t	len;
+	int		quoted;
 	int		ret;
-	int		i;
-	char	*tmp;
 
-	ret = SH_SUCCESS;
 	i = 0;
-	while (str[i] && str[i] != '$')
-		i++;
-	if (str[i] != '$')
-		return (SH_SUCCESS);
-	if ((str + i)[1] == '{')
-		ret = expand_brace_param(shell, str + i, result);
-	else
-		ret = expand_no_brace(shell, str + i, result);
-	tmp = *result;
-	str[i] = '\0';
-	if (ret != SH_SUCCESS)
+	len = 0;
+	quoted = 0;
+	ret = SH_SUCCESS;
+	while (str[i])
 	{
-		if (!(*result = ft_strdup(str)))
+		str[i] == '\'' ? quoted = !(quoted) : 0 ;
+		if (str[i] == '$' && !(is_char_escaped(str, i)) && quoted == 0 && ++i)
 		{
-			ft_strdel(&tmp);
-			return (SH_ERR_MALLOC);
+			ft_printf("ret = %d\n", is_charset(str[i], FIRST_CHAR));
+			if (str[i] == '{')
+				ret = expand_brace(shell, str + i, result, &len);
+			else if (!(is_charset(str[i], FIRST_CHAR)))
+				*result = "$";
+			else
+				ret = expand_no_brace(shell, str + i, result, &len);
+			if (ret != SH_SUCCESS
+				|| (ret = join_expanded(str, &i, result, len)) != SH_SUCCESS)
+				return (ret);
 		}
+		i++;
 	}
-	else if (!(*result = ft_strjoin(str, *result)))
-	{
-		ft_strdel(&tmp);
-		return (SH_ERR_MALLOC);
-	}
-	str[i] = '$';
-	ft_strdel(&tmp);
 	return (SH_SUCCESS);
 }
