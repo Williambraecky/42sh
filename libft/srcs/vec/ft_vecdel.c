fceae281566a0c025abb084c4507da2b00ab7d43
William Braeckman <wbraeckm@student.s19.be>
2020-01-15 17:42:40 +0100

    Fix ft_vecdel

--- libft/srcs/vec/ft_vecdel.c
+++ libft/srcs/vec/ft_vecdel.c
@@ -6,7 +6,7 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/17 18:58:17 by wbraeckm          #+#    #+#             */
-/*   Updated: 2019/06/17 19:01:08 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2020/01/15 17:07:39 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,11 @@ int		ft_vecdel(t_vec *vec, size_t pos)
 		return (1);
 	vec->vec[pos] = NULL;
 	while (pos < (vec->size_max - 1))
+	{
 		vec->vec[pos] = vec->vec[pos + 1];
+		pos++;
+	}
 	vec->vec[pos] = NULL;
 	vec->size--;
-	if (vec->size && vec->size == vec->size_max / 4)
-		ft_vecresize(vec, vec->size_max / 2);
 	return (0);
 }
