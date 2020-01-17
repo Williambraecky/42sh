5c2ea1358d1a06ccb37649ce0cc3c565f40377ed
William Braeckman <wbraeckm@student.s19.be>
2020-01-17 22:43:38 +0100

    Improve strjoin to allow for null entry

--- libft/srcs/str/ft_strjoin.c
+++ libft/srcs/str/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/12 12:19:22 by wbraeckm          #+#    #+#             */
-/*   Updated: 2018/06/18 10:37:57 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2020/01/17 22:41:43 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,18 @@ char	*ft_strjoin(const char *s1, const char *s2)
 	size_t	i;
 	char	*join;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
-	join = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
+	i = 0;
+	if (s1)
+		i += ft_strlen(s1);
+	if (s2)
+		i += ft_strlen(s2);
+	join = ft_strnew(i);
 	if (join == NULL)
 		return (NULL);
 	i = 0;
-	while (*s1)
+	while (s1 && *s1)
 		join[i++] = *s1++;
-	while (*s2)
+	while (s2 && *s2)
 		join[i++] = *s2++;
 	return (join);
 }
