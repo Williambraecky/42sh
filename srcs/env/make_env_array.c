619702f253cf365d312ac3201165d21b0e881aee
William Braeckman <wbraeckm@student.s19.be>
2020-01-17 22:53:11 +0100

    More proper env making function

--- srcs/env/make_env_array.c
+++ srcs/env/make_env_array.c
@@ -6,11 +6,39 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 17:30:07 by wbraeckm          #+#    #+#             */
-/*   Updated: 2020/01/13 01:21:15 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2020/01/17 16:47:23 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sh.h"
+
+/*
+** NOTE: this one doesn't allocate the array
+*/
+
+int		make_env_array_no_malloc(t_sh *shell, char **array)
+{
+	t_map	*env_map;
+	size_t	len;
+	size_t	i;
+	size_t	j;
+
+	env_map = shell->env;
+	len = ft_mapsize(shell->env);
+	i = 0;
+	j = 0;
+	while (i < env_map->max_size)
+	{
+		if (env_map->nodes[i].is_used)
+		{
+			if (!(array[j++] = ft_strformat("%s=%s", env_map->nodes[i].key,
+			(char*)env_map->nodes[i].value)))
+				return (SH_ERR_MALLOC);
+		}
+		i++;
+	}
+	return (SH_SUCCESS);
+}
 
 int		make_env_array(t_sh *shell, char ***array)
 {
--- srcs/exec/exec_proc.c
+++ srcs/exec/exec_proc.c
@@ -6,41 +6,36 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/10 16:32:41 by wbraeckm          #+#    #+#             */
-/*   Updated: 2020/01/16 02:20:52 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2020/01/17 17:47:27 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
 
 /*
-** TODO: those assigns need to be substituted aswell
+** TODO: we don't free proc->env here if the function fails
 */
 
-static int	make_new_env(t_sh *shell, t_proc *proc)
+static int	make_env(t_sh *shell, t_proc *proc)
 {
-	char	*current;
-	char	*tmp;
 	size_t	i;
+	size_t	j;
 
-	if (proc->assignments.size == 0)
-		return (SH_SUCCESS);
-	if (!(proc->env_backup = ft_mapclone(shell->env)))
+	if (!(proc->env = ft_memalloc(sizeof(char*) *
+		(shell->env->size + proc->assignments.size + 1))))
+		return (SH_ERR_MALLOC);
+	if (make_env_array_no_malloc(shell, proc->env) != SH_SUCCESS)
 		return (SH_ERR_MALLOC);
 	i = 0;
-	ft_memswap(&proc->env_backup, &shell->env, sizeof(t_map *));
-	while (i < proc->assignments.size)
+	while (proc->env[i])
+		i++;
+	j = 0;
+	while (j < proc->assignments.size)
 	{
-		current = (char*)ft_vecget(&proc->assignments, i++);
-		tmp = ft_strchr(current, '=');
-		*tmp = '\0';
-		if (repl_env(shell, current, tmp + 1) != SH_SUCCESS)
-		{
-			ft_memswap(&proc->env_backup, &shell->env, sizeof(t_map *));
-			ft_mapfilter(proc->env_backup, map_del_filter);
-			ft_mapdel(proc->env_backup);
+		if (!(proc->env[i++] =
+			ft_strdup((char*)ft_vecget(&proc->assignments, j))))
 			return (SH_ERR_MALLOC);
-		}
-		*tmp = '=';
+		j++;
 	}
 	return (SH_SUCCESS);
 }
@@ -52,7 +47,7 @@ static int	apply_assigns(t_sh *shell, t_proc *proc)
 	size_t	i;
 
 	if (proc->unprocessed_argv.size > 0)
-		return (make_new_env(shell, proc));
+		return (make_env(shell, proc));
 	i = 0;
 	while (i < proc->assignments.size)
 	{
@@ -96,13 +91,14 @@ int			exec_proc(t_sh *shell, t_proc *proc)
 
 	ret = proc_make_string(proc);
 	if (ret == SH_SUCCESS)
-		prepare_proc(shell, proc);
+		ret = prepare_proc(shell, proc);
 	if (ret == SH_SUCCESS)
 		ret = apply_assigns(shell, proc);
 	if (ret == SH_SUCCESS)
 		ret = proc_apply_redir(shell, proc);
-	if (ret == SH_SUCCESS)
-		ret = proc_exec_cmd(shell, proc);
+	if (ret != SH_SUCCESS)
+		proc->status = 1;
+	ret = proc_exec_cmd(shell, proc);
 	ret = undo_redir(proc, ret);
 	if (proc->env_backup && proc->env_backup->nodes)
 	{
