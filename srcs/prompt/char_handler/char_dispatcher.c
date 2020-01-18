0dee7847b103c898bf47810dc996a56199b73fb2
William Braeckman <wbraeckm@student.s19.be>
2020-01-18 18:28:31 +0100

    Implement ctrl+d completely

--- srcs/prompt/char_handler/char_dispatcher.c
+++ srcs/prompt/char_handler/char_dispatcher.c
@@ -6,7 +6,7 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/16 16:53:17 by wbraeckm          #+#    #+#             */
-/*   Updated: 2020/01/18 18:21:55 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2020/01/18 18:28:05 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,10 +33,22 @@ static int	char_ignore(t_prompt *prompt, char *buffer, t_sh *shell)
 	return (SH_SUCCESS);
 }
 
+/*
+** NOTE: if prompt is not empty remove character the cursor is sitting on
+*/
+
 static int	char_empty(t_prompt *prompt, char *buffer, t_sh *shell)
 {
 	(void)buffer;
 	(void)shell;
+	if (prompt->buffer.size)
+	{
+		if (prompt->buffer.buffer[prompt->buffer_index])
+			buff_remove(&prompt->buffer, ft_wstrindex(prompt->buffer.buffer,
+				prompt->char_index) - prompt->buffer.buffer);
+		print_from_cursor(prompt, NULL);
+		return (SH_SUCCESS);
+	}
 	buff_clear(&prompt->buffer);
 	if (buff_append(&prompt->buffer, "exit\n") != SH_SUCCESS)
 		return (SH_ERR_MALLOC);
