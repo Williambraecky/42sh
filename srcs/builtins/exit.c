41cd0fa5b2ef579743a3be67ff589cbb56fc311b
William Braeckman <wbraeckm@student.s19.be>
2020-01-18 17:59:40 +0100

    Don't exit if jobs are present and use absolute modul for exit code

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,14 +6,14 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/13 15:59:42 by wbraeckm          #+#    #+#             */
-/*   Updated: 2020/01/16 00:18:10 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2020/01/18 17:59:16 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 
 /*
-** TODO: define behaviour when jobs are running in background
+** TODO: define behaviour when jobs are stopped
 ** TODO: better handle arg reading
 */
 
@@ -24,8 +24,13 @@ int		exit_builtin(int argc, char **argv, t_sh *shell)
 	ret = 0;
 	if (argc > 1)
 		ret = ft_atoi(argv[1]);
+	if (shell->jobs.size != 0)
+	{
+		ft_dprintf(2, "42sh: exit: there are stopped jobs.\n");
+		return (1);
+	}
 	if (getpid() == shell->pid)
 		shell->running = 0;
-	shell->stop_code = ret;
+	shell->stop_code = (ret % 256 + 256) % 256;
 	return (0);
 }
