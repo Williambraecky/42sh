76fa05aaad13c47f09662e3e8c01f7c279b4542d
William Braeckman <wbraeckm@student.s19.be>
2020-01-21 21:12:37 +0100

    Rework alias: validated

--- srcs/builtins/alias.c
+++ srcs/builtins/alias.c
@@ -6,45 +6,14 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/16 15:01:51 by wbraeckm          #+#    #+#             */
-/*   Updated: 2020/01/14 18:30:56 by wdaher-a         ###   ########.fr       */
+/*   Updated: 2020/01/21 21:12:17 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sh.h"
 #include "lexer.h"
 
-/*
- ** TODO:
- **  no args:
- **   1: for every alias create a string in form key='value'{done}
- **   2: sort these string by ascii order{done}
- **   3: print them{done}
- **   4: ???
- **   5: profit
- **  with args, for every arg:
- **   1: split first arg on = (no need to actually split just replace = with \0)
- **   2: validate alias name (should be a-zA-Z0-9 probably)
- **   3: tokenize string after = (/!\ lexer should not ask for prompt again)
- **   4: add to alias (remove if it exists before)
- **   5: ???
- **   6: profit
- */
-
-int		valid_arg(char *string)
-{
-	char	*tmp;
-	int		ret;
-
-	tmp = ft_strchr(string, '=');
-	*tmp = '\0';
-	ret = 1;
-	if (!string || !str_is_name(string))
-		ret = 0;
-	*tmp = '=';
-	return (ret);
-}
-
-void	display(t_sh *shell, char **sorted_al)
+static void	display(t_sh *shell, char **sorted_al)
 {
 	size_t	i;
 
@@ -53,65 +22,85 @@ void	display(t_sh *shell, char **sorted_al)
 	i = 0;
 	while (sorted_al[i])
 	{
-		ft_printf("%s=%s\n", sorted_al[i], get_alias(shell, sorted_al[i]));
+		ft_printf("%s='%s'\n", sorted_al[i], get_alias(shell, sorted_al[i]));
 		++i;
 	}
 }
 
-int		print_aliases(t_sh *shell, t_map *al)
+static int	print_aliases(t_sh *shell, t_map *al)
 {
-	char	**al_tosort;
+	char	**array;
 	size_t	i;
 	size_t	j;
 	int		len;
 
-	len = shell->aliases->max_size;
-	if (!(al_tosort = (char **)malloc(sizeof(char *) * (len + 2))))
+	len = shell->aliases->size;
+	if (!(array = (char **)ft_memalloc(sizeof(char *) * (len + 1))))
 		return (SH_ERR_MALLOC);
-	i = 0;
+	i = al->max_size;
 	j = 0;
-	while (al_tosort && i < al->max_size)
+	while (i--)
 	{
 		if (al->nodes[i].is_used && j < (size_t)len)
 		{
-			if (!(al_tosort[j++] = ft_strdup(al->nodes[i].key)))
+			if (!(array[j++] = al->nodes[i].key))
+			{
+				free(array);
 				return (SH_ERR_MALLOC);
+			}
 		}
-		++i;
 	}
-	al_tosort[j] = NULL;
-	if (j > 1)
-		ft_strsort(al_tosort, j, &ft_strcmp);
-	display(shell, al_tosort);
-	ft_freesplit(al_tosort);
+	ft_strsort(array, j, &ft_strcmp);
+	display(shell, array);
+	free(array);
 	return (SH_SUCCESS);
 }
 
-int		alias_builtin(int argc, char **argv, t_sh *shell)
+static int	handle_define(t_sh *shell, char *definition)
 {
-	char	**tmp;
+	char	*equals;
+
+	equals = ft_strchr(definition, '=');
+	*equals = '\0';
+	if (repl_alias(shell, definition, equals + 1) != SH_SUCCESS)
+		return (SH_ERR_MALLOC);
+	return (SH_SUCCESS);
+}
+
+static int	handle_print(t_sh *shell, char *alias)
+{
+	char	*aliased;
+
+	aliased = get_alias(shell, alias);
+	if (!aliased)
+	{
+		ft_dprintf(2, "42sh: alias: %s: not found\n", alias);
+		return (1);
+	}
+	ft_printf("%s='%s'\n", alias, aliased);
+	return (0);
+}
+
+int			alias_builtin(int argc, char **argv, t_sh *shell)
+{
 	size_t	i;
+	int		err;
 	int		ret;
 
 	if (argc == 1)
 		return (print_aliases(shell, shell->aliases));
 	i = 1;
 	ret = SH_SUCCESS;
+	err = 0;
 	while (i < (size_t)argc && ret == SH_SUCCESS)
 	{
 		if (ft_strchr(argv[i], '=') != NULL)
-		{
-			if (!(tmp = ft_strsplit(argv[i], '=')) && valid_arg(argv[i]))
-			{
-				ft_putendl("spliterror");
-				return (SH_ERR_MALLOC);
-			}
-			ret = repl_alias(shell, tmp[0], tmp[1]);
-			ft_freesplit(tmp);
-		}
+			ret = handle_define(shell, argv[i]);
 		else
-			return (SH_ERR_SYNTAX);
-		++i;
+			err = handle_print(shell, argv[i]);
+		i++;
 	}
-	return (ret);
+	if (ret != SH_SUCCESS)
+		return (ret);
+	return (err);
 }
