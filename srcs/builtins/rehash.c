8d7b08b94b2eda020de18344e7c52c57a32f2c3b
William Braeckman <wbraeckm@student.s19.be>
2020-01-21 23:30:56 +0100

    Make rehash builtin

--- srcs/builtins/rehash.c
+++ srcs/builtins/rehash.c
@@ -6,7 +6,7 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/16 18:23:05 by wbraeckm          #+#    #+#             */
-/*   Updated: 2019/12/16 18:38:11 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2020/01/21 23:30:26 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,16 @@
 
 int		rehash_builtin(int argc, char **argv, t_sh *shell)
 {
+	size_t	i;
+
 	(void)argc;
 	(void)argv;
-	(void)shell;
+	i = 0;
+	while (i < shell->use_hash->max_size)
+	{
+		if (shell->use_hash->nodes[i].is_used)
+			remove_hash(shell, shell->use_hash->nodes[i].key);
+		i++;
+	}
 	return (0);
 }
