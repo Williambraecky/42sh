3304ed3fe565b83a1a2e7e43b436f240a84d8f7c
William Braeckman <wbraeckm@student.s19.be>
2020-01-21 22:15:20 +0100

    Rework export

--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -6,25 +6,11 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/16 18:21:03 by wbraeckm          #+#    #+#             */
-/*   Updated: 2020/01/15 17:49:32 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2020/01/21 22:15:04 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
-
-static int	valid_arg(char *string)
-{
-	char	*tmp;
-	int		ret;
-
-	tmp = ft_strchr(string, '=');
-	*tmp = '\0';
-	ret = 1;
-	if (!string || !str_is_name(string))
-		ret = 0;
-	*tmp = '=';
-	return (ret);
-}
 
 static void	print_env(t_map *map)
 {
@@ -41,9 +27,37 @@ static void	print_env(t_map *map)
 	}
 }
 
+static int	handle_define(t_sh *shell, char *definition)
+{
+	char	*equals;
+
+	equals = ft_strchr(definition, '=');
+	*equals = '\0';
+	if (!str_is_name(definition))
+	{
+		*equals = '=';
+		ft_dprintf(2, "42sh: export: `%s': not a valid identifier\n",
+			definition);
+		return (1);
+	}
+	if (repl_env(shell, definition, equals + 1) != SH_SUCCESS)
+		return (SH_ERR_MALLOC);
+	return (SH_SUCCESS);
+}
+
+static int	handle_export(t_sh *shell, char *key)
+{
+	char	*value;
+
+	if (get_internal(shell, key, &value) != SH_SUCCESS)
+		value = "";
+	if (repl_env(shell, key, value) != SH_SUCCESS)
+		return (SH_ERR_MALLOC);
+	return (SH_SUCCESS);
+}
+
 int			export_builtin(int argc, char **argv, t_sh *shell)
 {
-	char	**av;
 	size_t	i;
 	int		ret;
 
@@ -51,17 +65,13 @@ int			export_builtin(int argc, char **argv, t_sh *shell)
 		print_env(shell->env);
 	i = 1;
 	ret = SH_SUCCESS;
-	while (i < (size_t)argc && ret == SH_SUCCESS)
+	while (i < (size_t)argc)
 	{
-		if (ft_strchr(argv[i], '=') != NULL && valid_arg(argv[i]))
-		{
-			av = ft_strsplit(argv[i], '=');
-			ret = repl_env(shell, av[0], av[1]);
-			ft_freesplit(av);
-		}
+		if (ft_strchr(argv[i], '=') != NULL)
+			ret = handle_define(shell, argv[i]);
 		else
-			return (SH_ERR_SYNTAX);
-		++i;
+			ret = handle_export(shell, argv[i]);
+		i++;
 	}
 	return (ret);
 }
