81594f8f6c379c56d6d5b2bda344de1a0f069fa7
William Braeckman <wbraeckm@student.s19.be>
2020-01-22 22:50:51 +0100

    Make get_cursor_pos not break waiting input

--- srcs/utils/get_cursor_pos.c
+++ srcs/utils/get_cursor_pos.c
@@ -6,7 +6,7 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 16:44:34 by wbraeckm          #+#    #+#             */
-/*   Updated: 2020/01/18 15:51:46 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2020/01/22 22:50:24 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,27 +23,39 @@
 static int	read_pos_from_buffer(char *buffer, int *x, int *y)
 {
 	char	*tmp;
+	char	*write;
 	int		tmp_y;
 
 	if (!(tmp = ft_strchr(buffer, '[')))
 		return (2);
+	*tmp = '\0';
+	write = buffer;
+	while (write != tmp)
+		ioctl(0, TIOCSTI, write++);
 	tmp_y = ft_atoi(tmp + 1);
 	if (!(tmp = ft_strchr(tmp, ';')))
 		return (2);
 	*x = ft_atoi(tmp + 1);
+	tmp += ft_intlen(*x);
+	while (*tmp)
+		ioctl(0, TIOCSTI, tmp++);
 	*y = tmp_y;
 	return (0);
 }
 
+/*
+** TODO: whatever gets read needs to be put back
+*/
+
 int			get_cursor_pos(t_sh *sh, int *x, int *y)
 {
-	char	buf[30];
+	char	buf[31];
 	int		ret;
 	size_t	i;
 
 	if (sh->prompt_mode == 0)
 		return (1);
-	ft_memset(buf, 0, 30);
+	ft_memset(buf, 0, 31);
 	write(0, "\033[6n", 4);
 	i = 0;
 	while (i == 0 || (buf[i - 1] != 'R' && i < 30))
