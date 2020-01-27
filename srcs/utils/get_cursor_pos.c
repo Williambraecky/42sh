1ac324b5c41be8476d7a85fdaf22963a43016cd9
William Braeckman <wbraeckm@student.s19.be>
2020-01-28 00:05:07 +0100

    Change get_cursor_pos to better handle current stdin queue

--- srcs/utils/get_cursor_pos.c
+++ srcs/utils/get_cursor_pos.c
@@ -6,7 +6,7 @@
 /*   By: wbraeckm <wbraeckm@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 16:44:34 by wbraeckm          #+#    #+#             */
-/*   Updated: 2020/01/22 23:02:19 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2020/01/28 00:01:50 by wbraeckm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,51 +20,57 @@
 **	- 2: read error
 */
 
+static void	reinsert_in_buffer(char *buffer, char *end)
+{
+	char	buff;
+	int		queued;
+
+	if (ioctl(0, FIONREAD, &queued) == -1)
+		return ;
+	while (*buffer != '\e')
+		ioctl(0, TIOCSTI, buffer++);
+	while (*end)
+		ioctl(0, TIOCSTI, end++);
+	while (queued--)
+		read(0, &buff, 1);
+		ioctl(0, TIOCSTI, &buff);
+}
+
 static int	read_pos_from_buffer(char *buffer, int *x, int *y)
 {
 	char	*tmp;
-	char	*write;
 	int		tmp_y;
 
 	if (!(tmp = ft_strchr(buffer, '[')))
 		return (2);
-	*tmp = '\0';
-	write = buffer;
-	while (write != tmp - 1)
-		ioctl(0, TIOCSTI, write++);
 	tmp_y = ft_atoi(tmp + 1);
 	if (!(tmp = ft_strchr(tmp, ';')))
 		return (2);
 	*x = ft_atoi(tmp + 1);
-	tmp += ft_intlen(*x);
-	while (*tmp)
-		ioctl(0, TIOCSTI, tmp++);
+	tmp += ft_intlen(*x) + 2;
 	*y = tmp_y;
+	reinsert_in_buffer(buffer, tmp);
 	return (0);
 }
 
-/*
-** TODO: whatever gets read needs to be put back
-*/
-
 int			get_cursor_pos(t_sh *sh, int *x, int *y)
 {
-	char	buf[31];
+	char	buf[256];
 	int		ret;
 	size_t	i;
 
 	if (sh->prompt_mode == 0)
 		return (1);
-	ft_memset(buf, 0, 31);
+	ft_memset(buf, 0, 256);
 	write(0, "\033[6n", 4);
 	i = 0;
-	while (i == 0 || (buf[i - 1] != 'R' && i < 30))
+	while (i == 0 || (buf[i - 1] != 'R' && i < 255))
 	{
 		if (!(ret = read(0, buf + i, 1)))
 			return (2);
 		i++;
 	}
-	if (i < 2 || read_pos_from_buffer(buf, x, y) || i == 30)
+	if (i < 2 || read_pos_from_buffer(buf, x, y) || i == 256)
 		return (2);
 	return (0);
 }
