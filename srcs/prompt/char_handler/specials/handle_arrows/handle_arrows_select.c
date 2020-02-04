b8c815f3a8cb4b6b17cb33005256c397b9c91ad4
Maoro Pizzagalli <maoro.pizzagalli@gmail.com>
2020-02-04 16:00:55 +0100

    fix division by zero in select

--- srcs/prompt/char_handler/specials/handle_arrows/handle_arrows_select.c
+++ srcs/prompt/char_handler/specials/handle_arrows/handle_arrows_select.c
@@ -6,7 +6,7 @@
 /*   By: mpizzaga <mpizzaga@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/10 19:21:43 by mpizzaga          #+#    #+#             */
-/*   Updated: 2020/01/27 23:14:41 by wbraeckm         ###   ########.fr       */
+/*   Updated: 2020/02/04 15:42:35 by mpizzaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,19 @@ static void	select_handle_up_arrow(t_select *select)
 {
 	select->selected =
 		((select->selected - 1) % select->nb_elem + select->nb_elem) %
-		select->nb_elem;
+		ft_max(select->nb_elem, 1);
 }
 
 static void	select_handle_right_arrow(t_select *select)
 {
 	int real_row_total;
 
-	real_row_total = select->scroll ? select->nb_elem / select->elem_per_row + 1
+	real_row_total = select->scroll ? select->nb_elem /
+		ft_max(select->elem_per_row, 1) + 1
 	: select->row_total;
 	if (select->selected > ((real_row_total * select->elem_per_row)
 			- real_row_total - 1))
-		select->selected %= real_row_total;
+		select->selected %= ft_max(real_row_total, 1);
 	else
 	{
 		select->selected += real_row_total;
@@ -40,12 +41,13 @@ static void	select_handle_left_arrow(t_select *select)
 {
 	int real_row_total;
 
-	real_row_total = select->scroll ? select->nb_elem / select->elem_per_row + 1
+	real_row_total = select->scroll ? select->nb_elem /
+		ft_max(select->elem_per_row, 1) + 1
 	: select->row_total;
 	if (select->selected < real_row_total)
 	{
 		select->selected = (real_row_total * (select->elem_per_row - 1))
-			+ (select->selected % real_row_total);
+			+ (select->selected % ft_max(real_row_total, 1));
 		if (select->selected > select->nb_elem - 1)
 			select->selected = select->nb_elem - 1;
 	}
@@ -61,7 +63,7 @@ int			select_handle_arrows(t_prompt *prompt, char *buffer)
 	if (buffer[2] == 'A')
 		select_handle_up_arrow(select);
 	else if (buffer[2] == 'B')
-		select->selected = (select->selected + 1) % select->nb_elem;
+		select->selected = (select->selected + 1) % ft_max(select->nb_elem, 1);
 	else if (buffer[2] == 'C')
 		select_handle_right_arrow(select);
 	else if (buffer[2] == 'D')
